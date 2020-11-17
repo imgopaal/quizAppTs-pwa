4bff207c03a03429aa53509f386660c28b6e8220
GopalDas123 <Gopaldax@gmail.com>
2020-11-17 20:48:43 +0500

    Service Worker Added

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+let cacheData="appV1"
+this.addEventListener("install",(event)=>{
+    event.waitUntil(
+        caches.open(cacheData).then((cache)=>{
+            cache.addAll([
+                "/static/js/main.chunk.js",
+                "/static/js/bundle.js",
+                "/static/js/0.chunk.js",
+                "/index.html",
+                "/",
+
+            ])
+        })
+    )
+})
+
+this.addEventListener("fetch",(event)=>{
+    event.respondWith(
+        caches.match(event.request).then((resp)=>{
+            if(resp){
+                return resp
+            }
+        })
+    )
+})
