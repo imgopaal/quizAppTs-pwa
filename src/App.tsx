c0d71f38b963d1e4aeee9b971f2553c35cb6f55b
GopalDas123 <Gopaldax@gmail.com>
2020-11-17 20:16:09 +0500

    Initialized Project-7

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Quiz App With PWA
         </a>
       </header>
     </div>
